5614951a7d4a3b3ab1298f4bcef9067351261b13
Sunil <sunilsv26@gmail.com>
2020-10-18 10:57:43 +0530

    #4 init state with Posts,stored response data to Posts using setState in axios promise then block,rendered each post from state dynamically

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,32 +1,35 @@
-import React, { Component } from 'react';
-import axios from 'axios'
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import React, { Component } from "react";
+import axios from "axios";
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
 
 class Blog extends Component {
-    componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(response=>console.log(response))
-    }
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    Posts: [],
+  };
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => this.setState({ Posts: response.data }));
+  }
+  render() {
+    const posts = this.state.Posts.map((post) => {
+      return <Post title={post.title} key={post.id} />;
+    });
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
