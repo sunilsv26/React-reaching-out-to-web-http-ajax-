c14d026096ba284388b518d38ec5c97eb814c5e7
Sunil <sunilsv26@gmail.com>
2020-10-20 11:54:04 +0530

    #20 Added Navigation links for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import axios from '../../axios';
+import React, { Component,Fragment} from "react";
+import axios from "../../axios";
 import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
 import NewPost from "../../components/NewPost/NewPost";
@@ -8,53 +8,71 @@ import "./Blog.css";
 class Blog extends Component {
   state = {
     Posts: [],
-    selectedId:null,
-    title:null,
-    content:null,
-    err:false,
+    selectedId: null,
+    title: null,
+    content: null,
+    err: false,
   };
   componentDidMount() {
-    axios.get("/posts").then((response) => {
-      const posts = response.data.slice(0, 4);
-      const updatedPosts = posts.map((post) => {
-        return { ...post, author: "Max" };
-      });
-      return this.setState({ Posts: updatedPosts});
-    }).catch(error=>this.setState({err:true}));
+    axios
+      .get("/posts")
+      .then((response) => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map((post) => {
+          return { ...post, author: "Max" };
+        });
+        return this.setState({ Posts: updatedPosts });
+      })
+      .catch((error) => this.setState({ err: true }));
   }
 
-  selectedPostHandler=(id)=>{
-    this.setState({selectedId:id})
-  }
+  selectedPostHandler = (id) => {
+    this.setState({ selectedId: id });
+  };
 
-  postDeleteHandler=()=>{
-    this.setState({selectedId:null})
-  }
+  postDeleteHandler = () => {
+    this.setState({ selectedId: null });
+  };
   render() {
-    let posts = <p style={{textAlign:'center'}}>Somrthing Went Wrong</p>
-    if(!this.state.err){
+    let posts = <p style={{ textAlign: "center" }}>Somrthing Went Wrong</p>;
+    if (!this.state.err) {
       posts = this.state.Posts.map((post) => {
-        return <Post title={post.title} 
-        key={post.id} 
-        author={post.author}
-        clicked={()=>this.selectedPostHandler(post.id)}/>;
+        return (
+          <Post
+            title={post.title}
+            key={post.id}
+            author={post.author}
+            clicked={() => this.selectedPostHandler(post.id)}
+          />
+        );
       });
     }
     return (
-      <div>
-        <section className="Posts">
-          {posts}
-        </section>
+      <Fragment>
+        <header className="Blog">
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/">New-Post</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <div>
+          <section className="Posts">{posts}</section>
 
-        <section>
-          <FullPost id={this.state.selectedId} 
-          />
-        </section>
+          <section>
+            <FullPost id={this.state.selectedId} />
+          </section>
 
-        <section>
-          <NewPost />
-        </section>
-      </div>
+          <section>
+            <NewPost />
+          </section>
+        </div>
+      </Fragment>
     );
   }
 }
