660442a435c9a776d2b7d5bde368465550cf9cdd
Sunil <sunilsv26@gmail.com>
2020-10-20 12:58:34 +0530

    #23 using browserRouter in appJS

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,21 +1,9 @@
 import React, { Component, Fragment } from "react";
-import axios from "../../axios";
 import "./Blog.css";
 import Posts from "../Posts/Posts";
 
 class Blog extends Component {
-  componentDidMount() {
-    axios
-      .get("/posts")
-      .then((response) => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map((post) => {
-          return { ...post, author: "Max" };
-        });
-        return this.setState({ Posts: updatedPosts });
-      })
-      .catch((error) => this.setState({ err: true }));
-  }
+  
 
   postDeleteHandler = () => {
     this.setState({ selectedId: null });
